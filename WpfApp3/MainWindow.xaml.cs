4ec6523d9d32347940c2e6012bf6acbbac28d3d2
rakitinva <rakitinva@411D-14>
2024-10-23 12:14:14 +0700

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace MushroomApp
+{
+    public partial class MainWindow : Window
+    {
+        private List<Mushroom> mushrooms;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeMushrooms();
+            DisplayMushrooms();
+        }
+
+        private void InitializeMushrooms()
+        {
+            mushrooms = new List<Mushroom>
+            {
+               
+            };
+        }
+
+        private void DisplayMushrooms()
+        {
+            listBoxMushrooms.ItemsSource = null; // Сбрасываем источник
+            listBoxMushrooms.ItemsSource = mushrooms; // Устанавливаем обновленный источник
+        }
+
+        private void listBoxMushrooms_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (listBoxMushrooms.SelectedItem is Mushroom selectedMushroom)
+            {
+                textBoxId.Text = selectedMushroom.Id.ToString();
+                textBoxName.Text = selectedMushroom.Name;
+                textBoxColor.Text = selectedMushroom.Color;
+                checkBoxIsEdible.IsChecked = selectedMushroom.IsEdible;
+                textBoxWeight.Text = selectedMushroom.Weight.ToString();
+                textBoxHeight.Text = selectedMushroom.Height.ToString();
+                textBoxCapRadius.Text = selectedMushroom.CapRadius.ToString();
+            }
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBoxMushrooms.SelectedItem is Mushroom selectedMushroom)
+            {
+                selectedMushroom.Name = textBoxName.Text;
+                selectedMushroom.Color = textBoxColor.Text;
+                selectedMushroom.IsEdible = checkBoxIsEdible.IsChecked ?? false;
+                selectedMushroom.Weight = double.TryParse(textBoxWeight.Text, out var weight) ? weight : 0;
+                selectedMushroom.Height = double.TryParse(textBoxHeight.Text, out var height) ? height : 0;
+                selectedMushroom.CapRadius = double.TryParse(textBoxCapRadius.Text, out var radius) ? radius : 0;
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var newMushroom = new Mushroom
+            {
+                Id = mushrooms.Count + 1, // Простой способ генерации ID
+                Name = textBoxNewName.Text,
+                Color = textBoxNewColor.Text,
+                IsEdible = checkBoxNewIsEdible.IsChecked ?? false,
+                Weight = double.TryParse(textBoxNewWeight.Text, out var weight) ? weight : 0,
+                Height = double.TryParse(textBoxNewHeight.Text, out var height) ? height : 0,
+                CapRadius = double.TryParse(textBoxNewCapRadius.Text, out var radius) ? radius : 0
+            };
+
+            mushrooms.Add(newMushroom);
+            DisplayMushrooms();
+
+            // Очистка полей ввода
+            textBoxNewName.Clear();
+            textBoxNewColor.Clear();
+            checkBoxNewIsEdible.IsChecked = false;
+            textBoxNewWeight.Clear();
+            textBoxNewHeight.Clear();
+            textBoxNewCapRadius.Clear();
+        }
+    }
+}
---
+++ WpfApp3/Mushroom.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+namespace MushroomApp
+{
+    public class Mushroom : INotifyPropertyChanged
+    {
+        private int id;
+        private string name;
+        private string color;
+        private bool isEdible;
+        private double weight;
+        private double height;
+        private double capRadius;
+
+        public int Id
+        {
+            get => id;
+            set { id = value; OnPropertyChanged(nameof(Id)); }
+        }
+
+        public string Name
+        {
+            get => name;
+            set { name = value; OnPropertyChanged(nameof(Name)); }
+        }
+
+        public string Color
+        {
+            get => color;
+            set { color = value; OnPropertyChanged(nameof(Color)); }
+        }
+
+        public bool IsEdible
+        {
+            get => isEdible;
+            set { isEdible = value; OnPropertyChanged(nameof(IsEdible)); }
+        }
+
+        public double Weight
+        {
+            get => weight;
+            set { weight = value; OnPropertyChanged(nameof(Weight)); }
+        }
+
+        public double Height
+        {
+            get => height;
+            set { height = value; OnPropertyChanged(nameof(Height)); }
+        }
+
+        public double CapRadius
+        {
+            get => capRadius;
+            set { capRadius = value; OnPropertyChanged(nameof(CapRadius)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
